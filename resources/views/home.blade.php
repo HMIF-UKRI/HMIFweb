2d91a14a32845f8402a4f5abfa7a95cac6536055
Hanif Fathan <haniffathan33@gmail.com>
2025-05-30 20:32:48 +0700

    refactor: nambah UI kegiatan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.app")
 
 @section("content")
-    <body class="bg-white">
+    <div class="bg-white">
         <!-- Hero Section -->
         <section
             id="home"
@@ -110,20 +110,144 @@
                             <h3 class="mb-2 text-2xl font-bold text-white">
                                 Misi
                             </h3>
-                            <ul class="list-disc space-y-2 pl-6 text-gray-300">
-                                <li>
+                            <ul class="space-y-3">
+                                <li class="flex text-base">
+                                    <span class="mt-0.5 mr-2.5 text-red-500">
+                                        <svg
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 20 20"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <g
+                                                clip-path="url(#clip0_980_24852)"
+                                            >
+                                                <path
+                                                    d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
+                                                    fill="currentColor"
+                                                />
+                                                <path
+                                                    d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
+                                                    fill="currentColor"
+                                                />
+                                            </g>
+                                            <defs>
+                                                <clipPath id="clip0_980_24852">
+                                                    <rect
+                                                        width="20"
+                                                        height="20"
+                                                        fill="white"
+                                                    />
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </span>
                                     Mengembangkan potensi akademik dan
                                     non-akademik mahasiswa
                                 </li>
-                                <li>
+                                <li class="flex text-base">
+                                    <span class="mt-0.5 mr-2.5 text-red-500">
+                                        <svg
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 20 20"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <g
+                                                clip-path="url(#clip0_980_24852)"
+                                            >
+                                                <path
+                                                    d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
+                                                    fill="currentColor"
+                                                />
+                                                <path
+                                                    d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
+                                                    fill="currentColor"
+                                                />
+                                            </g>
+                                            <defs>
+                                                <clipPath id="clip0_980_24852">
+                                                    <rect
+                                                        width="20"
+                                                        height="20"
+                                                        fill="white"
+                                                    />
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </span>
                                     Membangun karakter kepemimpinan dan
                                     profesionalisme
                                 </li>
-                                <li>
+                                <li class="flex text-base">
+                                    <span class="mt-0.5 mr-2.5 text-red-500">
+                                        <svg
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 20 20"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <g
+                                                clip-path="url(#clip0_980_24852)"
+                                            >
+                                                <path
+                                                    d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
+                                                    fill="currentColor"
+                                                />
+                                                <path
+                                                    d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
+                                                    fill="currentColor"
+                                                />
+                                            </g>
+                                            <defs>
+                                                <clipPath id="clip0_980_24852">
+                                                    <rect
+                                                        width="20"
+                                                        height="20"
+                                                        fill="white"
+                                                    />
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </span>
                                     Meningkatkan solidaritas dan kerjasama antar
                                     mahasiswa
                                 </li>
-                                <li>
+                                <li class="flex text-base">
+                                    <span class="mt-0.5 mr-2.5 text-red-500">
+                                        <svg
+                                            width="20"
+                                            height="20"
+                                            viewBox="0 0 20 20"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <g
+                                                clip-path="url(#clip0_980_24852)"
+                                            >
+                                                <path
+                                                    d="M10 0.5625C4.78125 0.5625 0.5625 4.78125 0.5625 10C0.5625 15.2188 4.78125 19.4688 10 19.4688C15.2188 19.4688 19.4688 15.2188 19.4688 10C19.4688 4.78125 15.2188 0.5625 10 0.5625ZM10 18.0625C5.5625 18.0625 1.96875 14.4375 1.96875 10C1.96875 5.5625 5.5625 1.96875 10 1.96875C14.4375 1.96875 18.0625 5.59375 18.0625 10.0312C18.0625 14.4375 14.4375 18.0625 10 18.0625Z"
+                                                    fill="currentColor"
+                                                />
+                                                <path
+                                                    d="M12.6875 7.09375L8.96875 10.7188L7.28125 9.0625C7 8.78125 6.5625 8.8125 6.28125 9.0625C6 9.34375 6.03125 9.78125 6.28125 10.0625L8.28125 12C8.46875 12.1875 8.71875 12.2813 8.96875 12.2813C9.21875 12.2813 9.46875 12.1875 9.65625 12L13.6875 8.125C13.9688 7.84375 13.9688 7.40625 13.6875 7.125C13.4063 6.84375 12.9688 6.84375 12.6875 7.09375Z"
+                                                    fill="currentColor"
+                                                />
+                                            </g>
+                                            <defs>
+                                                <clipPath id="clip0_980_24852">
+                                                    <rect
+                                                        width="20"
+                                                        height="20"
+                                                        fill="white"
+                                                    />
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </span>
                                     Berperan aktif dalam kegiatan kemahasiswaan
                                     dan pengabdian masyarakat
                                 </li>
@@ -480,10 +604,36 @@
                 </div>
                 <div class="mt-8 text-center">
                     <a
-                        href="#"
-                        class="hover:bg-opacity-90 inline-block rounded-lg bg-red-900 px-6 py-3 font-bold text-white transition"
+                        class="group relative inline-flex items-center overflow-hidden rounded-lg border-2 border-current px-8 py-3 text-red-500 transition-colors duration-300 hover:text-white focus:ring-3 focus:outline-none"
+                        href="/struktur-pengurus"
                     >
-                        Lihat Semua Foto
+                        <span
+                            class="absolute top-0 left-0 z-0 h-full w-0 bg-red-900 transition-all duration-300 group-hover:w-full"
+                        ></span>
+                        <span
+                            class="absolute -end-full transition-all group-hover:end-4"
+                        >
+                            <svg
+                                class="size-5 shadow-sm rtl:rotate-180"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                />
+                            </svg>
+                        </span>
+
+                        <span
+                            class="z-10 text-sm font-medium transition-all group-hover:me-4"
+                        >
+                            Lihat Semua Galeri
+                        </span>
                     </a>
                 </div>
             </div>
@@ -601,5 +751,5 @@
                 </div>
             </div>
         </section>
-    </body>
+    </div>
 @endsection
