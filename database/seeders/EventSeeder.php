cfaa6ed4d89edeac1b73407b948cacf541cebce7
Hanif Fathan <haniffathan33@gmail.com>
2025-06-06 18:01:50 +0700

    refactor: update member & event seeder

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -4,10 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Event;
 use App\Models\EventCategory;
-use Carbon\Carbon;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class EventSeeder extends Seeder
@@ -17,69 +16,77 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-
         $categoryIds = EventCategory::pluck('id')->all();
 
         if (empty($categoryIds)) {
-            $this->command->error('No event categories found. Please run the EventCategorySeeder first or ensure categories exist.');
-
+            $this->command->error('No event categories found. Please run the EventCategorySeeder first.');
             return;
         }
+
+        $destinationPath = 'event/thumbnails';
+
+        Storage::disk('public')->deleteDirectory($destinationPath);
+        Storage::disk('public')->makeDirectory($destinationPath);
 
         $eventsData = [
             [
                 'title' => 'Click On Code',
-                'slug' => 'click-on-code',
-                'description' => 'A coding workshop for beginners.',
-                'thumbnail_path' => '/images/kegiatan/coc.jpg',
-                'event_date' => Date::create('2023-05-15 10:00:00'),
+                'thumbnail_path' => 'coc.jpg',
+                'short_description' => 'A coding workshop for beginners.',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante turpis. Maecenas sed enim eget magna scelerisque blandit eu in erat. Sed dapibus nisi ex, id faucibus elit varius quis. Nullam quis tempor ipsum, sed ultrices diam. Curabitur a lectus quis arcu pretium imperdiet ut vel ipsum. Suspendisse tincidunt purus sed felis viverra commodo. Donec congue et augue feugiat egestas. Vestibulum mi nulla, finibus vitae viverra imperdiet, ornare a ex. Vestibulum a arcu porta, luctus ipsum sed, hendrerit leo. Vestibulum posuere rutrum nibh, sed consequat purus ultrices in. Vestibulum mi urna, feugiat id aliquam ut, tempor eu risus. Donec nisl ante, ultrices sed sollicitudin sit amet, facilisis a lectus.',
+                'event_date' => Date::now(),
                 'location' => 'SMA 16 Bandung',
-                'status' => 'berlangsung',
-                'event_category_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
+                'status' => 'selesai',
             ],
             [
                 'title' => 'Sahur On The Road',
-                'slug' => 'sotr',
-                'description' => 'A community service event during Ramadan.',
-                'thumbnail_path' => 'images/kegiatan/sotr.jpg',
-                'event_date' => Date::create('2023-05-15 10:00:00'),
+                'thumbnail_path' => 'sotr.jpg',
+                'short_description' => 'A community service event during Ramadan.',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante turpis. Maecenas sed enim eget magna scelerisque blandit eu in erat. Sed dapibus nisi ex, id faucibus elit varius quis. Nullam quis tempor ipsum, sed ultrices diam. Curabitur a lectus quis arcu pretium imperdiet ut vel ipsum. Suspendisse tincidunt purus sed felis viverra commodo. Donec congue et augue feugiat egestas. Vestibulum mi nulla, finibus vitae viverra imperdiet, ornare a ex. Vestibulum a arcu porta, luctus ipsum sed, hendrerit leo. Vestibulum posuere rutrum nibh, sed consequat purus ultrices in. Vestibulum mi urna, feugiat id aliquam ut, tempor eu risus. Donec nisl ante, ultrices sed sollicitudin sit amet, facilisis a lectus.',
+                'event_date' => Date::now(),
                 'location' => 'Bandung City',
-                'status' => 'berlangsung',
-                'event_category_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
+                'status' => 'selesai',
             ],
             [
                 'title' => 'Diskusi Publik HMIF',
-                'slug' => 'diskusi-publik-hmif',
-                'description' => 'A public discussion event organized by HMIF.',
-                'thumbnail_path' => 'images/kegiatan/diskusi publik.jpg',
-                'event_date' => Date::create('2023-05-15 10:00:00'),
+                'thumbnail_path' => 'diskusi publik.jpg',
+                'short_description' => 'A public discussion event organized by HMIF.',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante turpis. Maecenas sed enim eget magna scelerisque blandit eu in erat. Sed dapibus nisi ex, id faucibus elit varius quis. Nullam quis tempor ipsum, sed ultrices diam. Curabitur a lectus quis arcu pretium imperdiet ut vel ipsum. Suspendisse tincidunt purus sed felis viverra commodo. Donec congue et augue feugiat egestas. Vestibulum mi nulla, finibus vitae viverra imperdiet, ornare a ex. Vestibulum a arcu porta, luctus ipsum sed, hendrerit leo. Vestibulum posuere rutrum nibh, sed consequat purus ultrices in. Vestibulum mi urna, feugiat id aliquam ut, tempor eu risus. Donec nisl ante, ultrices sed sollicitudin sit amet, facilisis a lectus.',
+                'event_date' => Date::now(),
                 'location' => 'Garasi Cempor',
-                'status' => 'berlangsung',
-                'event_category_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
+                'status' => 'selesai',
             ],
         ];
 
+        $dataToInsert = [];
+
         foreach ($eventsData as $data) {
-            Event::create([
+            $sourceFilePath = database_path('seeders/images/' . $data['thumbnail_path']);
+
+            $newFileName = Str::slug($data['title']) . '.' . pathinfo($data['thumbnail_path'], PATHINFO_EXTENSION);
+            $newFilePath = $destinationPath . '/' . $newFileName;
+
+            if (file_exists($sourceFilePath)) {
+                Storage::disk('public')->put($newFilePath, file_get_contents($sourceFilePath));
+            }
+
+            $dataToInsert[] = [
                 'title' => $data['title'],
-                'slug' => Str::slug($data['slug'] ?: $data['title'] . '-' . time()),
+                'slug' => Str::slug($data['title']),
                 'description' => $data['description'],
-                'thumbnail_path' => $data['thumbnail_path'],
-                'event_date' => Carbon::parse($data['event_date']),
+                'short_description' => $data['short_description'],
+                'thumbnail_path' => $newFilePath,
+                'event_date' => $data['event_date'],
                 'location' => $data['location'],
                 'status' => $data['status'],
                 'event_category_id' => $categoryIds[array_rand($categoryIds)],
-                'created_at' => $data['created_at'],
-                'updated_at' => $data['updated_at'],
-            ]);
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
         }
 
-        $this->command->info(count($eventsData) . ' events have been seeded!');
+        Event::insert($dataToInsert);
+
+        $this->command->info(count($dataToInsert) . ' events have been seeded!');
     }
 }
--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MemberSeeder extends Seeder
 {
@@ -13,37 +14,72 @@ class MemberSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('members')->insert([
+        $destinationPath = 'members';
+
+        Storage::disk('public')->deleteDirectory($destinationPath);
+        Storage::disk('public')->makeDirectory($destinationPath);
+
+        $membersData = [
             [
                 'name' => 'Chriss Hendry Choong',
+                'image' => 'acong.png',
                 'student_id_number' => '20221310011',
-                'image' => '/images/pengurus/acong.png',
                 'position' => 'Ketua Himpunan',
                 'organization_period_id' => 1,
                 'department_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
             [
                 'name' => 'Muhammad Ikhsan Kamil',
+                'image' => 'ikhsan.png',
                 'student_id_number' => '20221310012',
-                'image' => '/images/pengurus/ikhsan.png',
                 'position' => 'Wakil Ketua Himpunan',
                 'organization_period_id' => 1,
                 'department_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
             [
                 'name' => 'Tania Cahyani Putri',
+                'image' => 'tania.png',
                 'student_id_number' => '20221310013',
-                'image' => '/images/pengurus/tania.png',
                 'position' => 'Sekretaris',
                 'organization_period_id' => 1,
                 'department_id' => 1,
+            ],
+            [
+                'name' => 'Siti Maisyaroh',
+                'image' => 'simai.png',
+                'student_id_number' => '20221310014',
+                'position' => 'Bendahara',
+                'organization_period_id' => 1,
+                'department_id' => 1,
+            ]
+        ];
+
+        $dataToInsert = [];
+
+        foreach ($membersData as $member) {
+            $sourceFilePath = database_path('seeders/images/' . $member['image']);
+
+            // Buat nama file baru yang lebih unik untuk menghindari konflik
+            $newFileName = Str::slug($member['name']) . '.' . pathinfo($member['image'], PATHINFO_EXTENSION);
+            $newFilePath = $destinationPath . '/' . $newFileName;
+
+            // Salin file dari source ke storage
+            if (file_exists($sourceFilePath)) {
+                Storage::disk('public')->put($newFilePath, file_get_contents($sourceFilePath));
+            }
+
+            $dataToInsert[] = [
+                'name' => $member['name'],
+                'student_id_number' => $member['student_id_number'],
+                'image' => $newFilePath,
+                'position' => $member['position'],
+                'organization_period_id' => $member['organization_period_id'],
+                'department_id' => $member['department_id'],
                 'created_at' => now(),
                 'updated_at' => now(),
-            ],
-        ]);
+            ];
+        }
+
+        DB::table('members')->insert($dataToInsert);
     }
 }
