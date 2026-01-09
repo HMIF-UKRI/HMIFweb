455a104f2c6c3adf08b79c69b01d13565c4f5c89
Hanif Fathan <haniffathan33@gmail.com>
2026-01-09 19:56:09 +0700

    refactor: data seed member + image 2

--- database/migrations/2025_05_30_140516_create_members_table.php
+++ database/migrations/2025_05_30_140516_create_members_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name', 50);
             $table->string('student_id_number', 20)->unique();
             $table->string('image');
-            $table->string('position', 30);
+            $table->string('position');
             $table->foreignId('organization_period_id')->constrained('organization_periods', 'id')->onDelete('cascade');
             $table->foreignId('department_id')->constrained('departments', 'id')->onDelete('cascade');
             $table->timestamps();
