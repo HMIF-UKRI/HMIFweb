120935f54101e82c742b2aea6555d224d14c8773
harismy <harismynga@gmail.com>
2026-08-27 21:50:22 +0700

    Update error logs

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,5 @@
-@extends("errors.layout")
-@section("title", "Server Error")
-@section("code", "500")
-@section("message", "Something Went Wrong")
-@section
-('description', 'Maaf, terjadi kesalahan pada server kami. Tim kami sedang
-bekerja
+@extends('errors.layout')
+@section('title', 'Server Error')
+@section('code', '500')
+@section('message', 'Something Went Wrong')
+@section('description', 'Maaf, terjadi kesalahan pada server kami. Tim kami sedang bekerja memperbaikinya. Silakan coba lagi nanti.')
